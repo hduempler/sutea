076c5550342995b3e7d15affb30ef30b5e7fee1e
ChrisBee <Chris@cokuss.de>
2016-07-19 10:00:34 +0200

    leichte verbesserungen

--- sudoku_Teacher/src/sudoku/SudokuTeacherDemo.java
+++ sudoku_Teacher/src/sudoku/SudokuTeacherDemo.java
@@ -2,13 +2,42 @@ package sudoku;
 
 public class SudokuTeacherDemo {
 
+	public static int boxValue(int row, int col) {
+		int result = (row / 3) * 3 + (col / 3);
+		return result;
+	}
+
 	public static void main(String[] args) {
-		//ein Puzzle holen (derzeit noch einfach ohne DAO)
-		Spielfeld sf = new Spielfeld(1);
-		for (int j = 1; j < 10; j++) {
-			System.out.println(sf.rowVal[j]);
+		MyBitSet[] bisRow = new MyBitSet[9];
+		MyBitSet[] bisCol = new MyBitSet[9];
+		MyBitSet[] bisBox = new MyBitSet[9];
+		// ein Puzzle holen (derzeit noch einfach ohne DAO)
+		PuzzleArray game = new PuzzleArray(1);
+
+		for (int i = 0; i < 9; i++) {
+			bisBox[i] = new MyBitSet(9);
+			bisCol[i] = new MyBitSet(9);
+			bisRow[i] = new MyBitSet(9);
 		}
-		
+
+		for (int row = 0; row < 9; row++) {
+			for (int col = 0; col < 9; col++) {
+				bisRow[row].checkedSet(game.feld[row][col].getValue());
+				bisCol[row].checkedSet(game.feld[col][row].getValue());
+				bisBox[boxValue(row, col)].checkedSet(game.feld[row][col].getValue());
+			}
+		}
+		for (int i = 0; i < 9; i++) {
+			System.out.println("Die Box[" + i + "]: " + bisBox[i]);
+			System.out.println("Die Reihe[" + i + "]: " + bisRow[i]);
+			System.out.println("Die Spalte[" + i + "]: " + bisCol[i]);
+		}
+
+		Spielfeld sf = new Spielfeld(1);
+		for (int j = 0; j < 9; j++) {
+			System.out.println(bisRow[j]);
+		}
+
 	}
 
 }
